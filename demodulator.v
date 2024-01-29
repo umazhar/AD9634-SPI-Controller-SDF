0b7f04517f8effa0ecbace3e651d15ca8548aa60
umazhar <u.mazhar@icloud.com>
2024-01-28 23:33:21 -0800

    added mod, swapped demod/mod

--- demodulator.v
+++ demodulator.v
@@ -1,42 +1,30 @@
-module qpsk_modulator(
+module qpsk_demodulator(
     input clk,
     input rst_n,
-    input enable,
-    input [1:0] data_in, 
-    output reg signed [11:0] I_out, 
-    output reg signed [11:0] Q_out  
+    input signed [11:0] I_in, 
+    input signed [11:0] Q_in,
+    output reg [1:0] data_out
 );
 
-localparam [11:0] SYMBOL_MAX_POS = 12'h0801; // Represents cos(45 deg) and sin(45 deg) for 12-bit     2047
-localparam [11:0] SYMBOL_MAX_NEG = 12'h07FF; // Represents cos(45 deg) and sin(45 deg) for 12-bit     -2047
+localparam [11:0] SYMBOL_THRESHOLD = 12'h000; //threshold for pos/neg
 
 always @(posedge clk or negedge rst_n) begin
     if (!rst_n) begin
-        I_out <= 0;
-        Q_out <= 0;
-    end else if (enable) begin
-        case (data_in)
-            2'b00: begin
-                //45
-                I_out <= SYMBOL_MAX_POS; 
-                Q_out <= SYMBOL_MAX_POS;
+        data_out <= 2'b00;
+    end else begin
+        if (I_in > SYMBOL_THRESHOLD) begin
+            if (Q_in > SYMBOL_THRESHOLD) begin
+                data_out <= 2'b00; 
+            end else begin
+                data_out <= 2'b10; 
             end
-            2'b01: begin
-                //135
-                I_out <= SYMBOL_MAX_NEG; 
-                Q_out <= SYMBOL_MAX_POS; 
+        end else begin
+            if (Q_in > SYMBOL_THRESHOLD) begin
+                data_out <= 2'b01; 
+            end else begin
+                data_out <= 2'b11; 
             end
-            2'b10: begin
-                //225
-                I_out <= SYMBOL_MAX_POS; 
-                Q_out <= SYMBOL_MAX_NEG; 
-            end
-            2'b11: begin
-                //315
-                I_out <= SYMBOL_MAX_NEG; 
-                Q_out <= SYMBOL_MAX_NEG; 
-            end
-        endcase
+        end
     end
 end
 
